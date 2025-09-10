6fce26d1cb45ff2535ec956b40e254b04f615054
Samuel Alves Domínguez <samueladad@gmail.com>
2025-09-10 14:25:49 +0200

    sync

--- scripts/unit.gd
+++ scripts/unit.gd
@@ -14,6 +14,11 @@ class_name Unit
 	Constants.SLOT.LEGS : null
 }
 
+@export var passives: Dictionary
+
+@export var buffs: Dictionary
+@export var debuffs: Dictionary
+
 var skills: Array[Skill]
 
 var current_stats: Dictionary[Constants.STAT, int] = {
@@ -24,6 +29,18 @@ var current_stats: Dictionary[Constants.STAT, int] = {
 	Constants.STAT.WIT: 0,
 }
 
+func get_total_stat(stat: Constants.STAT) -> void:
+	var stat_total: int = 0
+	var item_bonus: int = 0
+	for item : Equipment in equipment.values():
+		for bonus in item.stat_modifiers:
+			pass
 
-func set_total_stats() -> void:
-	pass
+
+func set_current_stats() -> void:
+	var stat_total: int = 0
+	var item_bonus: int = 0
+	for stat in Constants.STAT:
+		for item in equipment:
+			pass
+		stat = stats.get_stat(stat)
