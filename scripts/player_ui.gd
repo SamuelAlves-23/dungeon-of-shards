9a74380be9ecc635f3edbd79a5f43937dde5bfbb
Samuel Alves Domínguez <samueladad@gmail.com>
2025-09-25 18:14:35 +0200

    Skill bar population 2

--- scripts/player_ui.gd
+++ scripts/player_ui.gd
@@ -3,9 +3,12 @@ class_name PlayerUI
 
 @onready var bar_amount: int = 3
 @onready var bar_slots: int = 9
-@onready var skills: Array
-
-@onready var current_skills: Array
+@onready var skill_bars: Dictionary = {
+	"1" : [],
+	"2" : [],
+	"3" : []
+}
+@onready var current_bar: int
 
 func _on_end_turn_button_pressed() -> void:
 	Signals.turn_ended.emit()
@@ -14,3 +17,9 @@ func populate_bar(new_skills: Array) -> void:
 	for skill in new_skills:
 		var new_btn: SkillButton = Constants.skilll_btn_scene.instantiate()
 		new_btn.skill = skill
+
+func manual_addition(skill: Skill, slot: int) -> void:
+	var new_btn: SkillButton = Constants.skilll_btn_scene.instantiate()
+	new_btn.skill = skill
+	skill_bars[current_bar].insert(slot, skill)
+	
